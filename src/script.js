60c74421f9670d9468117ec3ba2915028905bcf7
Beaver Bryan Antipolo <beaverbryanantipolo@gmail.com>
2020-12-30 22:47:47 -0500

    refactored winning conditions and minimax

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 'use strict'
+//list of mutable DOM elements
 const DOM = (() => {
     const tttScreen = document.getElementById('ttt-screen');
     const selectOpponent = document.querySelectorAll('.selectOpp');
     const selectOppCont = document.getElementById('choose-opponent-container');
     const selectTurn = document.querySelectorAll('.select-move');
     const playerTurn = document.getElementById('player-turn');
-    const playerName = document.getElementById('player-name');
     const oppTurn = document.getElementById('op-turn');
     const oppName = document.getElementById('op-name');
     const leftArrow = document.querySelector('.fa-arrow-left');
@@ -18,7 +18,6 @@ const DOM = (() => {
         selectOppCont,
         selectTurn,
         playerTurn,
-        playerName,
         oppName,
         oppTurn,
         leftArrow,
@@ -27,96 +26,129 @@ const DOM = (() => {
         displayText,
     }
 })();
-
+//player factory function
 const player = () => {
-    let ttt; //first move or 2nd move
-    let opponent; //either 2nd player or AI
-    const setOp = (value) => opponent = value; //setters
-    const setPlayerTurn = (turn) => ttt = turn;
-    const getName = () => player.caller.name; //getters
-    const getTTT = () => ttt;
-    const getOpponent = () => opponent;
-    const debugFunc = () => {
-        console.log(opponent);
-        console.log(getOpponent());
-        console.log(getTTT());
-    }
+    let _ttt = 'none'; //first move or 2nd move
+    let _opponent; //either 2nd player or AI
+    const setOp = (value) => _opponent = value; //setters
+    const setPlayerTurn = (turn) => _ttt = turn;
+    const getTTT = () => _ttt;
+    const getOpponent = () => _opponent;
     return {
-        debugFunc,
         setOp,
         setPlayerTurn,
-        getName,
         getTTT,
         getOpponent,
     }
 }
+
 const gameBoard = (() => {
-    let index = 0;
-    let board = ['', '', '', '', '', '', '', '', ''];
-    const setIndex = (v) => index += v;
-    const getIndex = () => index;
-    const resetIndex = () => index = 0; //resets the current index
-    const resetBoard = () => board.forEach((x, i, arr) => arr[i] = ''); //reset to default board
+    let _index = 0;
+    let _previousMove;
+    let board = ['', '', '',
+        '', '', '',
+        '', '', ''];
+    const setIndex = (v) => _index += v;
+    const getIndex = () => _index;
+    const resetIndex = () => _index = 0; //resets the current index
+    //reset to default board
+    const resetBoard = () => board.forEach((x, i, arr) => arr[i] = '');
+    const isBoardEmpty = () => board.every(x => '');
+    const isBoardFull = () => getBoardArr().every(x => x === "x" || x === "o");
     const getBoard = (i) => board[i];//get board element based on array index
     const getBoardArr = () => board; //get the board array
+    const getPreviousMove = () => _previousMove;
     const setTile = (v, i) => board[i] = v; //set board value by array index
-    const winCon = () => {
-        if ((getBoard(0) == Player.getTTT() && getBoard(1) == Player.getTTT() && getBoard(2) == Player.getTTT()) ||
-            (getBoard(3) == Player.getTTT() && getBoard(4) == Player.getTTT() && getBoard(5) == Player.getTTT()) ||
-            (getBoard(6) == Player.getTTT() && getBoard(7) == Player.getTTT() && getBoard(8) == Player.getTTT()) ||
-            (getBoard(0) == Player.getTTT() && getBoard(3) == Player.getTTT() && getBoard(6) == Player.getTTT()) ||
-            (getBoard(1) == Player.getTTT() && getBoard(4) == Player.getTTT() && getBoard(7) == Player.getTTT()) ||
-            (getBoard(2) == Player.getTTT() && getBoard(5) == Player.getTTT() && getBoard(8) == Player.getTTT()) ||
-            (getBoard(0) == Player.getTTT() && getBoard(4) == Player.getTTT() && getBoard(8) == Player.getTTT()) ||
-            (getBoard(2) == Player.getTTT() && getBoard(4) == Player.getTTT() && getBoard(6) == Player.getTTT())) {
-            return true;
+    const setPreviousMove = (v) => _previousMove = v;
+    const confirmWinner = () => {
+        let winner = 0;
+
+        //checks the winning condition
+        if (isEqual(getBoard(0), getBoard(1), getBoard(2)) || isEqual(getBoard(3), getBoard(4), getBoard(5)) ||
+            isEqual(getBoard(6), getBoard(7), getBoard(8)) || isEqual(getBoard(0), getBoard(3), getBoard(6)) ||
+            isEqual(getBoard(1), getBoard(4), getBoard(7)) || isEqual(getBoard(2), getBoard(5), getBoard(8)) ||
+            isEqual(getBoard(0), getBoard(4), getBoard(8)) || isEqual(getBoard(2), getBoard(4), getBoard(6))) {
+            getPreviousMove() == Player.getTTT()
+                ? winner = Player.getTTT()
+                : getPreviousMove() == Player2.getTTT()
+                    ? winner = Player2.getTTT()
+                    : winner = AI.getTTT();
         }
-        else if ((getBoard(0) == Player2.getTTT() && getBoard(1) == Player2.getTTT() && getBoard(2) == Player2.getTTT()) ||
-            (getBoard(3) == Player2.getTTT() && getBoard(4) == Player2.getTTT() && getBoard(5) == Player2.getTTT()) ||
-            (getBoard(6) == Player2.getTTT() && getBoard(7) == Player2.getTTT() && getBoard(8) == Player2.getTTT()) ||
-            (getBoard(0) == Player2.getTTT() && getBoard(3) == Player2.getTTT() && getBoard(6) == Player2.getTTT()) ||
-            (getBoard(1) == Player2.getTTT() && getBoard(4) == Player2.getTTT() && getBoard(7) == Player2.getTTT()) ||
-            (getBoard(2) == Player2.getTTT() && getBoard(5) == Player2.getTTT() && getBoard(8) == Player2.getTTT()) ||
-            (getBoard(0) == Player2.getTTT() && getBoard(4) == Player2.getTTT() && getBoard(8) == Player2.getTTT()) ||
-            (getBoard(2) == Player2.getTTT() && getBoard(4) == Player2.getTTT() && getBoard(6) == Player2.getTTT()) ||
-            (getBoard(0) == AI.getTTT() && getBoard(1) == AI.getTTT() && getBoard(2) == AI.getTTT()) ||
-            (getBoard(3) == AI.getTTT() && getBoard(4) == AI.getTTT() && getBoard(5) == AI.getTTT()) ||
-            (getBoard(6) == AI.getTTT() && getBoard(7) == AI.getTTT() && getBoard(8) == AI.getTTT()) ||
-            (getBoard(0) == AI.getTTT() && getBoard(3) == AI.getTTT() && getBoard(6) == AI.getTTT()) ||
-            (getBoard(1) == AI.getTTT() && getBoard(4) == AI.getTTT() && getBoard(7) == AI.getTTT()) ||
-            (getBoard(2) == AI.getTTT() && getBoard(5) == AI.getTTT() && getBoard(8) == AI.getTTT()) ||
-            (getBoard(0) == AI.getTTT() && getBoard(4) == AI.getTTT() && getBoard(8) == AI.getTTT()) ||
-            (getBoard(2) == AI.getTTT() && getBoard(4) == AI.getTTT() && getBoard(6) == AI.getTTT())) {
-            return false;
+        return winner;
+    }
+    const twoOutOfThree = (a, b, c) => {
+        return a == b && b == c && a != '';
+    }
+    const isEqual = (a, b, c) => {
+        if (a !== '') {
+            return a == b && b == c && c == a;
         }
     }
-    //const 
+
+    //check if there's two out of three equals to control the next move of AI
+    const minimaxResult = () => {
+        let winner = null;
+        if (twoOutOfThree(getBoard(0), getBoard(1), getBoard(2))) {
+            winner = getBoard(0);
+        }
+        if (twoOutOfThree(getBoard(3), getBoard(4), getBoard(5))) {
+            winner = getBoard(3);
+        }
+        if (twoOutOfThree(getBoard(6), getBoard(7), getBoard(8))) {
+            winner = getBoard(6);
+        }
+        if (twoOutOfThree(getBoard(0), getBoard(3), getBoard(6))) {
+            winner = getBoard(0);
+        }
+        if (twoOutOfThree(getBoard(1), getBoard(4), getBoard(7))) {
+            winner = getBoard(1);
+        }
+        if (twoOutOfThree(getBoard(2), getBoard(5), getBoard(8))) {
+            winner = getBoard(2);
+        }
+        if (twoOutOfThree(getBoard(0), getBoard(4), getBoard(8))) {
+            winner = getBoard(0);
+        }
+        if (twoOutOfThree(getBoard(2), getBoard(4), getBoard(6))) {
+            winner = getBoard(2);
+        }
+        if (isBoardFull() && winner == null) {
+            return winner = 't';
+        } else {
+            return winner;
+        }
+    }
     const tiles = document.querySelectorAll('.tiles');
     const clearBoard = () => {
         tiles.forEach((x) => x.textContent = "");
         resetBoard();
     }
     return {
-        index, tiles,
-        getIndex, setIndex,
-        winCon, resetIndex,
+        tiles, getIndex, setIndex,
+        confirmWinner, resetIndex,
         clearBoard, setTile,
         getBoard, resetBoard,
-        getBoardArr,
+        getBoardArr, isBoardEmpty,
+        isBoardFull, minimaxResult,
+        setPreviousMove, getPreviousMove,
+        twoOutOfThree, isEqual,
     }
 })();
 
 //display elements or display related (including event listeners) of this website
 const displayController = (() => {
+
     //screen transitions
     const startTTT = () => {
         DOM.tttScreen.setAttribute('style', 'display: initial');
         DOM.selectOppCont.setAttribute('style', 'visibility: hidden');
     }
+
     const chooseOpponentScreen = () => {
         DOM.tttScreen.setAttribute('style', 'display: none');
         DOM.selectOppCont.setAttribute('style', 'visibility: visible');
     }
+
     //display both parties' turn
     const displayPlayerTurn = () => {
         DOM.playerTurn.textContent = 'Your turn';
@@ -124,25 +156,25 @@ const displayController = (() => {
         DOM.leftArrow.style.visibility = 'visible';
         DOM.rightArrow.style.visibility = 'hidden';
     }
+
     const displayPlayer2Turn = () => {
         DOM.playerTurn.textContent = 'Next turn';
         DOM.oppTurn.textContent = 'Your turn';
         DOM.leftArrow.style.visibility = 'hidden';
         DOM.rightArrow.style.visibility = 'visible';
     }
+
     const determineWinner = () => {
-        if (gameBoard.winCon()) {
-            console.log('I WIN')
+        if (gameBoard.confirmWinner() == Player.getTTT()) {
             DOM.displayModal.style.display = 'initial';
             DOM.displayText.textContent = 'Player1 wins!!'
         }
-        else if (gameBoard.winCon() === false) {
+        else if (gameBoard.confirmWinner() == Player2.getTTT() ||
+            gameBoard.confirmWinner() == AI.getTTT()) {
             DOM.displayModal.style.display = 'initial';
             DOM.displayText.textContent = `${Player.getOpponent()} wins!!`;
-            DOM.displayText.textContent = `${Player.getOpponent()} wins!!`;
-            DOM.displayText.textContent = `${Player.getOpponent()} wins!!`;
         }
-        else if (gameBoard.getBoardArr().every(x => x === "x" || x === "o")) {
+        else if (gameBoard.isBoardFull()) {
             DOM.displayModal.style.display = 'initial';
             DOM.displayText.textContent = 'It\'s a Tie!!';
         }
@@ -151,14 +183,24 @@ const displayController = (() => {
     //website start - choose your opponent
     for (const el of DOM.selectOpponent) {
         el.addEventListener('click', (e) => {
-            e.target.id === 'playerOpp' || e.target.id === 'iconPlayer'
-                ? (Player.setOp('Player2'), DOM.oppName.textContent = "Player2", Player.setPlayerTurn('x'),
-                    Player2.setPlayerTurn('o'))
-                : (Player.setOp('AI'), DOM.oppName.textContent = "The Unbeatable A.I"
-                    , Player.setPlayerTurn('x'), AI.setPlayerTurn('o'));
+            if (e.target.id === 'playerOpp' || e.target.id === 'iconPlayer') {
+                Player.setOp('Player2');
+                DOM.oppName.textContent = "Player2";
+                Player.setPlayerTurn('x');
+                Player2.setPlayerTurn('o');
+                gameBoard.resetIndex();
+                gameBoard.clearBoard();
+            }
+            else {
+                Player.setOp('AI');
+                DOM.oppName.textContent = "The Unbeatable A.I";
+                Player.setPlayerTurn('o');
+                AI.setPlayerTurn('x');
+                gameBoard.resetIndex();
+                gameBoard.clearBoard();
+                comAI.AImove([...gameBoard.tiles]);
+            }
             startTTT();
-            gameBoard.resetIndex();
-            gameBoard.clearBoard();
         });
     }
     //allows the user to return to the intro screen(choosing opponent)
@@ -172,6 +214,12 @@ const displayController = (() => {
         Player.getTTT() === 'x'
             ? displayPlayerTurn()
             : displayPlayer2Turn();
+        console.log(Player.getOpponent());
+        console.log(AI.getTTT());
+        if (Player.getOpponent() == 'AI' && AI.getTTT() == 'x') {
+            comAI.AImove([...gameBoard.tiles]);
+            displayController.displayPlayerTurn();
+        }
     }
     return {
         startTTT,
@@ -181,6 +229,17 @@ const displayController = (() => {
         determineWinner,
     }
 })()
+const comAI = (() => {
+    const AImove = (arr) => {
+        let moveIndex = findBestMove();
+        //AI move
+        arr[moveIndex].textContent = AI.getTTT();
+        gameBoard.setPreviousMove(AI.getTTT());
+        gameBoard.setTile(AI.getTTT(), moveIndex);
+    }
+    return { AImove };
+})()
+
 //factory function declared as a variable
 const Player = player();
 const Player2 = player();
@@ -196,12 +255,14 @@ const AI = player();
             if (e.target.textContent === "" && Player.getOpponent() === 'Player2') {
                 if (isEven(gameBoard.getIndex())) {
                     e.target.textContent = 'x';
+                    gameBoard.setPreviousMove('x');
                     gameBoard.setTile('x', i);
                     Player.getTTT() === 'x' ? displayController.displayPlayer2Turn()
                         : displayController.displayPlayerTurn();
                 }
                 else {
                     e.target.textContent = 'o';
+                    gameBoard.setPreviousMove('o');
                     gameBoard.setTile('o', i);
                     Player.getTTT() === 'x' ? displayController.displayPlayerTurn()
                         : displayController.displayPlayer2Turn();
@@ -209,18 +270,17 @@ const AI = player();
                 gameBoard.setIndex(1);
             }
             else if (e.target.textContent === "" && Player.getOpponent() === 'AI') {
-                Player.getTTT() === 'x' //activates if first move
-                    ? (e.target.textContent = 'x',
-                        displayController.displayPlayer2Turn(),
-                        gameBoard.setTile('x', i))
-                    : (e.target.textContent = 'o',
-                        displayController.displayPlayerTurn(),
-                        gameBoard.setTile('o', i));
-                let moveIndex = findBestMove();
-                AI.getTTT() === 'x'
-                    ? (arr[moveIndex].textContent = 'x', gameBoard.setTile('x', moveIndex))
-                    : (arr[moveIndex].textContent = 'o', gameBoard.setTile('o', moveIndex));
+                e.target.textContent = Player.getTTT();
+                displayController.displayPlayer2Turn();
+                gameBoard.setTile(Player.getTTT(), i);
+                gameBoard.setPreviousMove(Player.getTTT());
+                /*prevents the hindrance of displaying the result
+                and avoids producing an error within minimax by stopping 
+                when the board is full already*/
                 console.log(gameBoard.getBoardArr());
+                if (!gameBoard.isBoardFull()) {
+                    comAI.AImove(arr);
+                }
             }
             displayController.determineWinner();
         });
@@ -243,8 +303,12 @@ const AI = player();
                 Player2.setPlayerTurn('x');
                 AI.setPlayerTurn('x');
                 displayController.displayPlayer2Turn();
+                gameBoard.clearBoard();
+                if (Player.getOpponent() == 'AI') {
+                    comAI.AImove([...gameBoard.tiles]);
+                    displayController.displayPlayerTurn();
+                }
             }
         });
     }
 })()
-
